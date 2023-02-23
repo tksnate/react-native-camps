ab0c7c787ab1dad051ff0ceb2e4889555a0d26a2
tksnate <tksnate@gmail.com>
2023-02-22 18:41:16 -0700

    activity indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,9 @@
 import { ScrollView, Text, View } from "react-native";
 import { Card, ListItem, Avatar } from "react-native-elements";
-import { PARTNERS } from "../shared/partners";
-import { useState } from "react";
+import { State } from "react-native-gesture-handler";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const Mission = () => {
   return (
@@ -22,7 +24,33 @@ const Mission = () => {
 };
 
 const AboutScreen = () => {
-  const [partners, setPartners] = useState(PARTNERS);
+  const partners = useSelector((state) => state.partners);
+
+  if (partners.isLoading) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Loading />
+        </Card>
+      </ScrollView>
+    );
+  }
+
+  if (partners.errMess) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Text>{partners.errMess}</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
 
   return (
     <ScrollView>
@@ -30,9 +58,9 @@ const AboutScreen = () => {
       <Card>
         <Card.Title>Community Partners</Card.Title>
         <Card.Divider />
-        {partners.map((partner) => (
+        {partners.partnersArray.map((partner) => (
           <ListItem key={partner.id}>
-            <Avatar source={partner.image} rounded />
+            <Avatar source={{ uri: baseUrl + partner.image }} rounded />
             <ListItem.Content>
               <ListItem.Title>{partner.name}</ListItem.Title>
               <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,28 +1,39 @@
-import { FlatList } from "react-native";
-import { Avatar, ListItem } from "react-native-elements";
-import { useState } from "react";
-import { CAMPSITES } from "../shared/campsites";
+import { FlatList, Text, View } from "react-native";
+import { Tile } from "react-native-elements";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const DirectoryScreen = ({ navigation }) => {
-  const [campsites, setCampsites] = useState(CAMPSITES);
+  const campsites = useSelector((state) => state.campsites);
+
+  if (campsites.isLoading) {
+    return <Loading />;
+  }
+
+  if (campsites.errMess) {
+    return (
+      <View>
+        <Text>{campsites.errMess}</Text>
+      </View>
+    );
+  }
 
   const renderDirectoryItem = ({ item: campsite }) => {
     return (
-      <ListItem
+      <Tile
+        title={campsites.name}
+        caption={campsite.description}
+        featured
         onPress={() => navigation.navigate("CampsiteInfo", { campsite })}
-      >
-        <Avatar source={campsite.image} rounded />
-        <ListItem.Content>
-          <ListItem.Title>{campsite.name}</ListItem.Title>
-          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
+        imageSrc={{ uri: baseUrl + campsite.image }}
+      />
     );
   };
 
   return (
     <FlatList
-      data={campsites}
+      data={campsites.campsitesArray}
       renderItem={renderDirectoryItem}
       keyExtractor={(item) => item.id.toString()}
     />
